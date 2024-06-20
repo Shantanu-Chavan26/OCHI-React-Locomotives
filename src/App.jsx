be9f875cdebd734ac828aca9f4ca17dd5919c2b7
Shantanu <shantanuchavan2603@gmail.com>
2024-06-20 15:20:22 +0530

    Eyes.jsx creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
+import Eyes from './components/Eyes'
 function App() {
   return (
     <div className='w-full min-h-screen  text-white bg-zinc-900'>
@@ -10,6 +11,7 @@ function App() {
       <LandingPage />
       <Marquee />
       <About />
+      <Eyes /> 
     </div>
   )
 }
