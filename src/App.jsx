4fdffb64b4d1f58951a13febbbc7da08d602cdf7
Shantanu <shantanuchavan2603@gmail.com>
2024-06-24 11:02:05 +0530

    Animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import LandingPage from './components/LandingPage'
-import Marquee from './components/Marquee'
-import About from './components/About'
-import Eyes from './components/Eyes'
-import Featured from './components/Featured'
+import React from 'react';
+import { motion, useScroll } from "framer-motion";
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import Marquee from './components/Marquee';
+import About from './components/About';
+import Eyes from './components/Eyes';
+import Featured from './components/Featured';
+
 function App() {
+  const { scrollYProgress } = useScroll();
+
   return (
-    <div className='w-full min-h-screen  text-white bg-[#F7F3E9]'>
+    <div className='relative w-full min-h-screen text-white bg-[#F7F3E9]'>
+      {/* Scroll Progress Indicator */}
+      <motion.div
+        className='fixed top-0 left-0 right-0 h-1 bg-blue-400 origin-left z-50'
+        style={{ scaleX: scrollYProgress }}
+      />
+      
       <Navbar />
       <LandingPage />
       <Marquee />
       <About />
-      <Eyes /> 
+      <Eyes />
       <Featured />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,31 +1,141 @@
 import React from 'react';
+import { motion } from "framer-motion";
 
 function Featured() {
   return (
     <div className='w-full py-10 bg-zinc-900'>
-      <div className='w-full px-10 border-b-2 pb-20'>
-        <h1 className='text-7xl font-neue-montreal text-white'>Featured Projects</h1>
+      <div className='w-full px-10 border-b-2 border-white pb-20'>
+        <h1 className='text-7xl font-serif text-semibold text-white'>Featured Projects</h1>
       </div>
       <div className='px-20 mt-10'>
-        <div className='cardcontainer w-full flex gap-10 relative'>
+        <div className='cardcontainer w-full flex justify-content: space-around flex-grow-10 gap-10 relative align-content:space-arund'>
           {/* Left Card */}
-          <div className='relative card w-1/2 h-[75vh] overflow-hidden'>
-            <h1 className='absolute z-10 text-6xl leading-none tracking-tighter font-neue-montreal text-white w-full text-center top-[50%] left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 py-2 mr-10'>
-              Project 1
-            </h1>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001
+              }
+            }}
+            className='leftcard relative card w-1/2 h-[75vh] overflow-hidden'
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileHover={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className='absolute z-10 text-5xl leading-none tracking-tighter font-neue-montreal text-[#b3fb21] w-full text-center top-[50%] left-1/2 transform -translate-x-1/2 py-2 mr-10'
+            >
+              {"PROJECT 1".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </motion.div>
+
             <div className='rounded-xl w-full h-full overflow-hidden'>
               <img className="w-full h-full object-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="Project 1" />
             </div>
-          </div>
+          </motion.div>
+
+          
+
+         
           {/* Right Card */}
-          <div className='relative card w-1/2 h-[75vh] overflow-hidden'>
-            <h1 className='absolute z-10 text-6xl leading-none tracking-tighter font-neue-montreal text-white w-full text-center top-5 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 py-2'>
-              Project 2
-            </h1>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001
+              }
+            }}
+            className='rightcard relative card w-1/2 h-[75vh] overflow-hidden rounded-xl  border-2 border-white p-4'
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileHover={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className='absolute z-10 text-5xl leading-none tracking-tighter font-neue-montreal text-[#b3fb21] w-full text-center top-[50%] left-1/2 transform -translate-x-1/2 py-2 '
+            >
+              {"PROJECT 2".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </motion.div>
+
             <div className='rounded-xl w-full h-full overflow-hidden'>
               <img className="w-full h-full object-cover" src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="Project 2" />
             </div>
-          </div>
+          </motion.div>
+        </div>
+        <div className='cardcontainer w-full flex justify-content: space-around flex-grow-10 gap-10 relative align-content:space-arund mt-20'>
+          {/* Left Card */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001
+              }
+            }}
+            className='leftcard relative card w-1/2 h-[75vh] overflow-hidden'
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileHover={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className='absolute z-10 text-5xl leading-none tracking-tighter font-neue-montreal text-[#b3fb21] w-full text-center top-[50%] left-1/2 transform -translate-x-1/2 py-2 mr-10'
+            >
+              {"PROJECT 3".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </motion.div>
+
+            <div className='rounded-xl w-full h-full overflow-hidden'>
+              <img className="w-full h-full object-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="Project 1" />
+            </div>
+          </motion.div>
+
+          
+
+          
+
+          {/* Right Card */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001
+              }
+            }}
+            className='rightcard relative card w-1/2 h-[75vh] rounded-xl overflow-hidden border-2 border-white p-4'
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileHover={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className='absolute z-10 text-5xl leading-none tracking-tighter font-neue-montreal text-[#b3fb21] w-full text-center top-[50%] left-1/2 transform -translate-x-1/2 py-2'
+            >
+              {"PROJECT 4".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </motion.div>
+
+            <div className='rounded-xl w-full h-full overflow-hidden'>
+              <img className="w-full h-full object-cover" src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="Project 2" />
+            </div>
+          </motion.div>
         </div>
       </div>
     </div>
